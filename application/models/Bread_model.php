c8f56b72e1ff0f70469068ce4c8ba72e7fd27df2
GMT <gyrome2.itracksolutions@gmail.com>
2019-05-31 10:35:50 +0800

    From: Toolkit_model To: Bread_model -Added Bread_model to Autoload

--- application/models/Bread_model.php
+++ application/models/Bread_model.php
@@ -1,29 +1,30 @@
 <?php
 
-class Toolkit_model extends CI_MODEL{
+class Bread_model extends CI_MODEL {
 
+	/* Functions Below are Used for Reference para may kopyahan ng Codes */
 	function get_schools()
 	{
-		$sql = "SELECT 
-		 			`id`, 
+		$sql = "SELECT
+		 			`id`,
 		 			`school_name`
 					FROM `schools`";
 
         $result = $this->db->query($sql);
         return $result->result_array();
 	}
-	
+
 	function get_school_rep($school_id)
 	{
-		$sql = "SELECT 
-		 			`id`, 
+		$sql = "SELECT
+		 			`id`,
 		 			`rep_name`
 					FROM `representatives` WHERE school_id = $school_id";
 
         $result = $this->db->query($sql);
         return $result->result_array();
 	}
-	
+
 	function get_rep_survey($rep_id)
 	{
 		$sql = "SELECT id, DATE_FORMAT(created_at, '%a, %M %d %Y %h:%i %p') as 'Date' FROM `survey_result` WHERE rep_id = $rep_id;";
@@ -31,12 +32,12 @@ class Toolkit_model extends CI_MODEL{
         $result = $this->db->query($sql);
         return $result->result_array();
 	}
-	
+
 	function get_rep_survey_details($survey_id)
 	{
-		$sql = "SELECT sd.survey_id, 
-						q.question, 
-						sd.answer_point 
+		$sql = "SELECT sd.survey_id,
+						q.question,
+						sd.answer_point
 						FROM survey_details sd inner join questions q on sd.question_id = q.id WHERE sd.survey_id = $survey_id";
 
         $result = $this->db->query($sql);
@@ -45,9 +46,9 @@ class Toolkit_model extends CI_MODEL{
 
 	function get_categories()
 	{
-		$sql = "SELECT 
-		 			`id`, 
-		 			`category_name`, 
+		$sql = "SELECT
+		 			`id`,
+		 			`category_name`,
 		 			`is_deleted`
 					FROM `categories` WHERE is_deleted = 0";
 
@@ -57,10 +58,10 @@ class Toolkit_model extends CI_MODEL{
 
 	function get_questions($id)
 	{
-		$sql = "SELECT 
-		 			* 
-		 			FROM questions 
-		 			WHERE category_id = $id 
+		$sql = "SELECT
+		 			*
+		 			FROM questions
+		 			WHERE category_id = $id
 		 			AND is_deleted = 0";
 
         $result = $this->db->query($sql);
@@ -69,10 +70,10 @@ class Toolkit_model extends CI_MODEL{
 
 	function get_answers($id)
 	{
-		$sql = "SELECT 
-		 			* 
-		 			FROM answers 
-		 			WHERE question_id = $id 
+		$sql = "SELECT
+		 			*
+		 			FROM answers
+		 			WHERE question_id = $id
 		 			AND is_deleted = 0";
 
         $result = $this->db->query($sql);
@@ -81,12 +82,12 @@ class Toolkit_model extends CI_MODEL{
 
 	function get_questionnaire($id)
 	{
-		$sql = "SELECT c.category_name, 
-					q.question, 
-					a.answers, 
-					a.val, 
+		$sql = "SELECT c.category_name,
+					q.question,
+					a.answers,
+					a.val,
 					c.id AS category_id,
-					q.id AS question_id, 
+					q.id AS question_id,
 					a.id AS answer_id
 					FROM answers a
 					LEFT OUTER JOIN questions q
@@ -94,19 +95,19 @@ class Toolkit_model extends CI_MODEL{
 					LEFT OUTER JOIN categories c
 					ON c.id = q.category_id
 					WHERE c.is_deleted = 0
-					AND q.is_deleted = 0 
+					AND q.is_deleted = 0
 					AND a.is_deleted = 0
 					AND c.id = $id";
 
         $result = $this->db->query($sql);
         return $result->result_array();
 	}
-	
+
 	function set_schoolname($schoolname, $schoolrep)
 	{
-		$check_sql = "SELECT 
+		$check_sql = "SELECT
 		 			id
-		 			FROM schools 
+		 			FROM schools
 		 			WHERE school_name = '$schoolname'";
 		$check_result = $this->db->query($check_sql);
 		if($check_result->num_rows() == 0){
@@ -117,10 +118,10 @@ class Toolkit_model extends CI_MODEL{
 		} else {
 			$school_id = $check_result->row('id');
 		}
-		
-		$check_rep_sql = "SELECT 
+
+		$check_rep_sql = "SELECT
 		 			*
-		 			FROM representatives 
+		 			FROM representatives
 		 			WHERE rep_name = '$schoolrep' and school_id = (SELECT id from schools where school_name = '$schoolname')";
 		$check_rep_result = $this->db->query($check_rep_sql);
 		if($check_rep_result->num_rows() == 0){
@@ -132,31 +133,31 @@ class Toolkit_model extends CI_MODEL{
 			return "Failed";
 		}
 	}
-	
+
 	function set_survey_result($school_name, $schoolrep)
 	{
 		$sql = "INSERT INTO survey_result(rep_id, created_at) VALUES ((SELECT id from representatives where rep_name = '$schoolrep' and school_id = (SELECT id from schools where school_name = '$school_name')), NOW())";
 		$result = $this->db->query($sql);
 		return $this->db->insert_id();
 	}
-	
+
 	function set_survey_details($survey_id, $question, $point)
 	{
 		$sql = "INSERT INTO survey_details(survey_id, question_id, answer_point) VALUES ('$survey_id', '$question', '$point')";
 		$result = $this->db->query($sql);
 	}
-	
+
 	function get_survey_details($survey_id)
 	{
-		$sql = "SELECT 
-		 			* 
-		 			FROM survey_details 
+		$sql = "SELECT
+		 			*
+		 			FROM survey_details
 		 			WHERE survey_id = $survey_id";
 
         $result = $this->db->query($sql);
         return $result->result_array();
 	}
-	
+
 	function get_survey_result_detailed($survey_id, $points)
 	{
 		$sql = "SELECT c.id as category_id, c.`category_name`, q.id as question_id, q.`question`, a.`answers`
